473d9aaad72c9cd1edb6607f725801459713c104
Christopher Baek <christopher.baek@gmail.com>
2016-03-18 21:42:21 -0700

    On a request to the home page, start a new game

--- game-server.js
+++ game-server.js
@@ -1,13 +1,16 @@
 (function(exports) {
+	// Import Socket.IO and attach it to export
 	var io = require('socket.io')();
 	exports.io = io;
 
-	exports.hello = function() {
-		console.log("hello");
-	}
-
+	// Set up Socket.IO
 	io.on('connection', function(socket) {
 		console.log('someone connected!');
 	});
+
+	// Start a new game
+	exports.startNewGame = function() {
+		return 69;
+	}
 
 })(module.exports);
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,15 @@ module.exports = function(game) {
   var express = require('express');
   var router = express.Router();
 
-  /* GET home page. */
-  router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+  // Generate a new game ID and bounce the user to that game
+  router.get('/', function(request, response) {
+  	var gameId = game.startNewGame();
+    response.redirect('/' + gameId);
+  });
+
+  // Handle users in a game
+  router.get('/:gameId', function(request, response) {
+  	response.render('index', { title: 'Express' });
   });
 
   return router;
