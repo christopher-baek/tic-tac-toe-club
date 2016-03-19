78c8346db66f3b79efafb5b575d1d9ec5114c3f7
Christopher Baek <christopher.baek@gmail.com>
2016-03-18 21:27:21 -0700

    Clean up spacing and remove unused Users routes

--- app.js
+++ app.js
@@ -5,15 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-//var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // import the game and attach it to the app
 var game = require('./game-server');
 app.game = game;
 
+// import the routes
 var routes = require('./routes/index')(game);
 
 // view engine setup
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,11 @@
 module.exports = function(game) {
-	var express = require('express');
-	var router = express.Router();
+  var express = require('express');
+  var router = express.Router();
 
-	/* GET home page. */
-	router.get('/', function(req, res, next) {
-		game.hello();
-	  res.render('index', { title: 'Express' });
-	});
+  /* GET home page. */
+  router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+  });
 
-	return router;
+  return router;
 }
